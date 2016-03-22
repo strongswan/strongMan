97ee5b080ef3347765c413f5dc1a3e384838c348
Murthy10 <samuel.kurath@gmail.com>
2016-03-22 13:44:57 +0100

    first throw of dockerfile

--- strongMan/tests/connections/test_connections_models.py
+++ strongMan/tests/connections/test_connections_models.py
@@ -7,7 +7,7 @@ class ConnectionModelTest(TestCase):
     def setUp(self):
         typ = Typ(name="IKEv2 Certificate")
         typ.save()
-        
+
         connection = Connection(profile='rw', auth='pubkey', version=1, typ=typ)
         connection.save()
 
