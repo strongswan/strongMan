7ae2207b9379c2a6f8137b47ef9e599186017a8e
Murthy10 <samuel.kurath@gmail.com>
2016-05-09 09:43:28 +0200

    removed connection_ urls

--- strongMan/apps/connections/urls.py
+++ strongMan/apps/connections/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'connections'
 urlpatterns = [
     url(r'^$', views.overview, name='index'),
-    url(r'^add/$', views.create, name='connections_choose'),
-    url(r'^(?P<id>\d+)/$', views.update, name='connection_update'),
-    url(r'delete/(?P<id>\d+)/$', views.delete_connection, name='connection_delete'),
-    url(r'state/(?P<id>\d+)/$', views.get_state, name='connection_state'),
-    url(r'log/$', views.get_log, name='connection_log'),
-    url(r'toggle/$', views.toggle_connection, name='connection_toggle'),
-    url(r'info/$', views.get_sa_info, name='connection_info'),
+    url(r'^add/$', views.create, name='choose'),
+    url(r'^(?P<id>\d+)/$', views.update, name='update'),
+    url(r'delete/(?P<id>\d+)/$', views.delete_connection, name='delete'),
+    url(r'state/(?P<id>\d+)/$', views.get_state, name='state'),
+    url(r'log/$', views.get_log, name='log'),
+    url(r'toggle/$', views.toggle_connection, name='toggle'),
+    url(r'info/$', views.get_sa_info, name='info'),
     url(r'identities/$', views.get_identities, name='identities'),
 ]
