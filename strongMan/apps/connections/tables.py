0990ae616751a794d5d5d2c6ac06f4d86972e64b
Severin Bühler <sevi_buehler@hotmail.com>
2016-05-11 10:09:27 +0200

    disabled orderable column state

--- strongMan/apps/connections/tables.py
+++ strongMan/apps/connections/tables.py
@@ -5,7 +5,7 @@ class ConnectionTable(tables.Table):
     name = tables.Column(accessor="profile", verbose_name='Name')
     gateway = tables.Column(accessor="remote_addresses.first.value", verbose_name="Server", orderable=False)
     typ = tables.Column(accessor="subclass.choice_name", verbose_name="Typ", orderable=False)
-    state = tables.Column(accessor="state", verbose_name="State", attrs={'th': {'class': "text-right"}, 'td': {'class': "text-right"}})
+    state = tables.Column(accessor="state", verbose_name="State", orderable=False, attrs={'th': {'class': "text-right"}, 'td': {'class': "text-right"}})
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
