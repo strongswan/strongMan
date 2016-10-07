f2706f714d539f28ebbdeca7bf8b5bb7bd2bc5ee
Josip Valencic <jvalenci@hsr.ch>
2016-10-07 13:06:51 +0200

    Login is required for promode

--- strongMan/apps/promode/views/__init__.py
+++ strongMan/apps/promode/views/__init__.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from .ProHandler import ProHandler
 
 
+@login_required
 @require_http_methods('GET')
 def promode(request):
     handler = ProHandler(request)
