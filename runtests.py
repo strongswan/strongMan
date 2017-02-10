16637bee90f7e8bea05b4c3ba67032575282e13f
Tobias Brunner <tobias@strongswan.org>
2017-02-10 15:31:27 +0100

    Run tests with pytest and add pep8 checks and coverage
    Also try to make sure to build from the correct repository and branch/commit.

---
+++ runtests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import sys
+import pytest
+import coverage
+
+if __name__ == '__main__':
+    # Environment variables
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strongMan.settings.local')
+
+    measure_coverage = True
+    if '--no-cov' in sys.argv:
+        measure_coverage = False
+        sys.argv.remove('--no-cov')
+
+    # Start coverage tracking
+    if measure_coverage:
+        cov = coverage.coverage()
+        cov.start()
+
+    # Run pytest
+    if len(sys.argv) > 1:
+        code = pytest.main(sys.argv)
+    else:
+        code = pytest.main(['strongMan/apps','strongMan/helper_apps','strongMan/tests/tests'])
+
+    # Show coverage report
+    if measure_coverage:
+        cov.stop()
+        cov.save()
+        cov.report()
+
+    sys.exit(code)
