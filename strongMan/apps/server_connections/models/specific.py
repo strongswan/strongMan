strongswan/strongMan

commit 52530499cfa19a961fe7db8869192f824e3d21ee

Server connections added

strongMan/apps/server_connections/models/specific.py
@@ -1,0 +1,82 @@
+from collections import OrderedDict
+
+from django.db import models
+from django.dispatch import receiver
+
+from strongMan.apps.certificates.models import UserCertificate, CertificateDoNotDelete, PrivateKey
+from strongMan.helper_apps.encryption import fields
+from .authentication import Authentication
+from .common import CertConDoNotDeleteMessage, KeyConDoNotDeleteMessage
+
+
+@receiver(UserCertificate.should_prevent_delete_signal, sender=UserCertificate)
+def prevent_cert_delete_if_cert_is_in_use(sender, **kwargs):
+    cert = kwargs['instance']
+    authentications = [ident.tls_identity for ident in cert.identities] + [ident.cert_identity for ident in
+                                                                           cert.identities] + [
+                          cert.ca_cert_authentication]
+
+    for auth in authentications:
+        if auth.count() > 0:
+            raise CertificateDoNotDelete(CertConDoNotDeleteMessage(auth.first().connection))
+    return False, ""
+
+
+@receiver(PrivateKey.should_prevent_delete_signal, sender=PrivateKey)
+def prevent_key_delete_if_cert_is_in_use(sender, **kwargs):
+    cert = kwargs['usercertificate']
+    authentications = [ident.tls_identity for ident in cert.identities] + [ident.cert_identity for ident in
+                                                                           cert.identities]
+    for auth in authentications:
+        if auth.count() > 0:
+            raise CertificateDoNotDelete(KeyConDoNotDeleteMessage(auth.first().connection))
+    return False, ""
+
+
+class Child(models.Model):
+    name = models.TextField()
+    mode = models.TextField()
+    connection = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None,
+                                   related_name='server_children')
+
+    def dict(self):
+        child = OrderedDict()
+        child['remote_ts'] = [remote_t.value for remote_t in self.remote_ts.all()]
+        child['esp_proposals'] = [esp_proposal.type for esp_proposal in self.esp_proposals.all()]
+        return child
+
+
+class Address(models.Model):
+    value = models.TextField()
+    local_ts = models.ForeignKey(Child, null=True, blank=True, default=None, related_name='server_local_ts')
+    remote_ts = models.ForeignKey(Child, null=True, blank=True, default=None, related_name='remote_ts')
+    remote_addresses = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None,
+                                         related_name='server_remote_addresses')
+    local_addresses = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None,
+                                        related_name='server_local_addresses')
+    vips = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None, related_name='server_vips')
+
+
+class Proposal(models.Model):
+    type = models.TextField()
+    child = models.ForeignKey(Child, null=True, blank=True, default=None, related_name='server_esp_proposals')
+    connection = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None,
+                                   related_name='server_proposals')
+
+
+class Secret(models.Model):
+    type = models.TextField()
+    data = fields.EncryptedCharField(max_length=50)
+    authentication = models.ForeignKey(Authentication, null=True, blank=True, default=None,
+                                       related_name='server_authentication')
+
+    def dict(self):
+        eap_id = self.authentication.subclass().eap_id
+        secrets = OrderedDict(type=self.type, data=self.data, id=eap_id)
+        return secrets
+
+
+class LogMessage(models.Model):
+    connection = models.ForeignKey("server_connections.Connection", null=True, blank=True, default=None)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()

strongMan/apps/server_connections/apps.py
@@ -1,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class serverConnectionsConfig(AppConfig):
+    name = 'server_connections'

strongMan/apps/server_connections/forms/ConnectionForms.py
@@ -1,0 +1,153 @@
+import sys
+
+from django import forms
+from strongMan.apps.server_connections.forms.SubForms import HeaderForm, CaCertificateForm, \
+    ServerIdentityForm, UserCertificateForm, EapForm, EapTlsForm
+from strongMan.apps.server_connections.models.connections import IKEv2Certificate, IKEv2EAP, IKEv2CertificateEAP, \
+    IKEv2EapTls
+
+
+class AbstractDynamicForm(forms.Form):
+    refresh_choices = forms.CharField(max_length=10, required=False)
+    current_form = forms.CharField(max_length=100, required=True)
+
+    @property
+    def current_form_class(self):
+        current_form_name = self.cleaned_data["current_form"]
+        return getattr(sys.modules[__name__], current_form_name)
+
+    @property
+    def template(self):
+        raise NotImplementedError
+
+    def update_certs(self):
+        pass
+
+
+class ChooseTypeForm(AbstractDynamicForm):
+    form_name = forms.ChoiceField()
+
+    def __init__(self, *args, **kwargs):
+        super(ChooseTypeForm, self).__init__(*args, **kwargs)
+        self.fields['form_name'].choices = ChooseTypeForm.get_choices()
+
+    @property
+    def selected_form_class(self):
+        name = self.cleaned_data["form_name"]
+        return getattr(sys.modules[__name__], name)
+
+    @property
+    def template(self):
+        return "server_connections/forms/ChooseType.html"
+
+    @classmethod
+    def get_choices(cls):
+        return tuple(
+            tuple((type(subclass()).__name__, subclass().model.choice_name)) for subclass in
+            AbstractConnectionForm.__subclasses__())
+
+
+class AbstractConnectionForm(AbstractDynamicForm):
+    def is_valid(self):
+        valid = True
+        # Call is_valid method for every base class
+        for base in self.__class__.__bases__:
+            if base == AbstractConnectionForm:
+                continue
+            if hasattr(base, "is_valid"):
+                valid = base.is_valid(self)
+                if not valid:
+                    valid = False
+        return valid
+
+    def create_connection(self):
+        connection = self.model(profile=self.cleaned_data['profile'], auth='pubkey', version=2)
+        connection.save()
+        # Call create_connection method for every base class
+        for base in self.__class__.__bases__:
+            if base == AbstractConnectionForm:
+                continue
+            if hasattr(base, "create_connection"):
+                base.create_connection(self, connection)
+        return connection
+
+    def update_connection(self, pk):
+        connection = self.model.objects.get(id=pk)
+        for base in self.__class__.__bases__:
+            if base == AbstractConnectionForm:
+                continue
+            if hasattr(base, "update_connection"):
+                base.update_connection(self, connection)
+        return connection
+
+    def fill(self, connection):
+        # Call fill method for every base class
+        for base in self.__class__.__bases__:
+            if base == AbstractConnectionForm:
+                continue
+            if hasattr(base, "fill"):
+                base.fill(self, connection)
+
+    @classmethod
+    def get_models(cls):
+        return tuple(tuple((subclass().model(), type(subclass()).__name__)) for subclass in cls.__subclasses__())
+
+    @classmethod
+    def subclass(self, connection):
+        typ = type(connection)
+        for model, form_name in self.get_models():
+            if type(model) == typ:
+                form_class = getattr(sys.modules[__name__], form_name)
+                return form_class()
+        return self
+
+
+class Ike2CertificateForm(AbstractConnectionForm, HeaderForm, UserCertificateForm, CaCertificateForm,
+                          ServerIdentityForm):
+    @property
+    def model(self):
+        return IKEv2Certificate
+
+    @property
+    def template(self):
+        return "server_connections/forms/Ike2Certificate.html"
+
+    def update_certs(self):
+        self.update_certificates()
+
+
+class Ike2EapForm(AbstractConnectionForm, HeaderForm, CaCertificateForm, ServerIdentityForm, EapForm):
+    @property
+    def model(self):
+        return IKEv2EAP
+
+    @property
+    def template(self):
+        return "server_connections/forms/Ike2EAP.html"
+
+
+class Ike2EapCertificateForm(AbstractConnectionForm, HeaderForm, UserCertificateForm, CaCertificateForm,
+                             ServerIdentityForm, EapForm):
+    @property
+    def model(self):
+        return IKEv2CertificateEAP
+
+    @property
+    def template(self):
+        return "server_connections/forms/Ike2EapCertificate.html"
+
+    def update_certs(self):
+        self.update_certificates()
+
+
+class Ike2EapTlsForm(AbstractConnectionForm, HeaderForm, CaCertificateForm, ServerIdentityForm, EapTlsForm):
+    @property
+    def model(self):
+        return IKEv2EapTls
+
+    @property
+    def template(self):
+        return "server_connections/forms/Ike2EapTls.html"
+
+    def update_certs(self):
+        self.update_certificates()

strongMan/apps/server_connections/forms/FormFields.py
@@ -1,0 +1,64 @@
+from django import forms
+from strongMan.apps.certificates.models import UserCertificate
+
+
+class CertificateChoice(forms.ModelChoiceField):
+    @property
+    def is_certificate_choice(self):
+        return True
+
+
+class IdentityChoiceValue:
+    def __init__(self, identity):
+        self.identity = identity
+
+    def __str__(self):
+        return str(self.identity)
+
+    def type(self):
+        return self.identity.type()
+
+
+class IdentityChoice(forms.ChoiceField):
+    def __init__(self, *args, **kwargs):
+        super(IdentityChoice, self).__init__(*args, **kwargs)
+        self._certificate = None
+
+    @property
+    def is_identity_choice(self):
+        return True
+
+    @property
+    def certificate(self):
+        return self._certificate
+
+    @certificate.setter
+    def certificate(self, value):
+        self._certificate = value
+        self.choices = IdentityChoice.to_choices(self._certificate.identities.all())
+
+    @classmethod
+    def load_identities(cls, form, certificate_field_name, identity_field_name):
+        data_source = {}
+        if not form.data == {}:
+            data_source = form.data
+        elif not form.initial == {}:
+            data_source = form.initial
+        else:
+            return
+        if not certificate_field_name in data_source:
+            return
+        cert_id = data_source[certificate_field_name]
+        cert = UserCertificate.objects.filter(pk=cert_id).first()
+        identity = form.fields[identity_field_name]
+        if not identity.certificate == cert and cert is not None:
+            identity.certificate = cert
+
+    @classmethod
+    def to_choices(cls, identity_queryset):
+        choices = []
+        for ident in identity_queryset:
+            subident = ident.subclass()
+            choice = (subident.pk, IdentityChoiceValue(subident))
+            choices.append(choice)
+        return choices

strongMan/apps/server_connections/forms/SubForms.py
@@ -1,0 +1,335 @@
+from django import forms
+from strongMan.apps.certificates.models import UserCertificate, AbstractIdentity
+from strongMan.apps.server_connections.models import Connection, Child, Address, Proposal, AutoCaAuthentication, \
+    CaCertificateAuthentication, CertificateAuthentication, EapAuthentication, Secret, EapTlsAuthentication
+from .FormFields import CertificateChoice, IdentityChoice
+
+
+class HeaderForm(forms.Form):
+    connection_id = forms.IntegerField(required=False)
+    profile = forms.CharField(max_length=50, initial="")
+    gateway = forms.CharField(max_length=50, initial="")
+
+    def clean_profile(self):
+        profile = self.cleaned_data['profile']
+        id = self.cleaned_data['connection_id']
+        if id is not None:
+            if Connection.objects.filter(profile=profile).exclude(pk=id).exists():
+                raise forms.ValidationError("Connection with same name already exists!")
+        elif Connection.objects.filter(profile=profile).exists():
+            raise forms.ValidationError("Connection with same name already exists!")
+        return profile
+
+    def fill(self, connection):
+        self.initial['profile'] = connection.profile
+        self.initial['gateway'] = connection.server_remote_addresses.first().value
+
+    def create_connection(self, connection):
+        child = Child(name=self.cleaned_data['profile'], connection=connection)
+        child.save()
+        self._set_proposals(connection, child)
+        self._set_addresses(connection, child, self.cleaned_data['gateway'])
+
+    def update_connection(self, connection):
+        Child.objects.filter(connection=connection).update(name=self.cleaned_data['profile'])
+        Address.objects.filter(remote_addresses=connection).update(value=self.cleaned_data['gateway'])
+        connection.profile = self.cleaned_data['profile']
+        connection.save()
+
+    def model(self):
+        raise NotImplementedError
+
+    def get_choice_name(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def _set_proposals(connection, child):
+        #Proposal(type="aes128-sha256-modp2048", connection=connection).save()
+        Proposal(type="default", connection=connection).save()
+        Proposal(type="aes128gcm128-modp2048", child=child).save()
+        #Proposal(type="default", child=child).save()
+
+    @staticmethod
+    def _set_addresses(connection, child, gateway):
+        Address(value=gateway, remote_addresses=connection).save()
+        Address(value='localhost', local_addresses=connection).save()
+        Address(value='0.0.0.0', vips=connection).save()
+        Address(value='::', vips=connection).save()
+        Address(value='::/0', remote_ts=child).save()
+        Address(value='0.0.0.0/0', remote_ts=child).save()
+
+
+class CaCertificateForm(forms.Form):
+    """
+    Manages the ca certificate field.
+    Contains a checkbox for 'auto choosing' the ca certificate and a select field for selecting the certificate manually.
+    Either the checkbox is checked or the certificate is selected.
+    """
+    certificate_ca = CertificateChoice(queryset=UserCertificate.objects.none(), label="CA certificate", required=False)
+    certificate_ca_auto = forms.BooleanField(initial=True, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(CaCertificateForm, self).__init__(*args, **kwargs)
+        self.fields['certificate_ca'].queryset = UserCertificate.objects.all()
+
+    def clean_certificate_ca(self):
+        if "certificate_ca_auto" in self.data:
+            return ""
+        if not "certificate_ca" in self.data:
+            raise forms.ValidationError("This field is required!")
+        identity_ca = self.data["certificate_ca"]
+        if identity_ca == "":
+            raise forms.ValidationError("This field is required!")
+        else:
+            return identity_ca
+
+    @property
+    def is_auto_choose(self):
+        return self.cleaned_data["certificate_ca_auto"]
+
+    @is_auto_choose.setter
+    def is_auto_choose(self, value):
+        self.initial['certificate_ca_auto'] = value
+
+    @property
+    def chosen_certificate(self):
+        pk = self.cleaned_data["certificate_ca"]
+        if pk == '':
+            return None
+        return UserCertificate.objects.get(pk=pk)
+
+    @chosen_certificate.setter
+    def chosen_certificate(self, value):
+        self.initial['certificate_ca'] = value
+
+    @classmethod
+    def ca_certificate_exists(cls):
+        exists = UserCertificate.objects.filter(is_CA=True).count() != 0
+        return exists
+
+    def fill(self, connection):
+        for remote in connection.server_remote.all():
+            sub = remote.subclass()
+            if isinstance(sub, AutoCaAuthentication):
+                self.is_auto_choose = True
+                break
+            if isinstance(sub, CaCertificateAuthentication):
+                self.chosen_certificate = sub.ca_cert.pk
+                self.is_auto_choose = False
+                break
+
+    def create_connection(self, connection):
+        if self.is_auto_choose:
+            AutoCaAuthentication(name='remote-cert', auth='pubkey', remote=connection).save()
+        else:
+            CaCertificateAuthentication(name='remote-cert', auth='pubkey', remote=connection,
+                                        ca_cert=self.chosen_certificate).save()
+
+    def update_connection(self, connection):
+        for remote in connection.server_remote.all():
+            sub = remote.subclass()
+            if isinstance(sub, CaCertificateAuthentication):
+                sub.delete()
+            if isinstance(sub, AutoCaAuthentication):
+                sub.delete()
+        if self.is_auto_choose:
+            AutoCaAuthentication(name='remote-cert', auth='pubkey', remote=connection).save()
+        else:
+            CaCertificateAuthentication(name='remote-cert', auth='pubkey', remote=connection,
+                                        ca_cert=self.chosen_certificate).save()
+
+
+class ServerIdentityForm(forms.Form):
+    """
+    Manages the server identity field.
+    Containes a checkbox to take the gateway field as identity and a field to fill a own identity.
+    Either the checkbox is checked or a own identity is field in the textbox.
+
+    """
+    identity_ca = forms.CharField(max_length=200, label="Server identity", required=False, initial="")
+    is_server_identity = forms.BooleanField(initial=True, required=False)
+
+    def clean_identity_ca(self):
+        if "is_server_identity" in self.data:
+            return ""
+        if not "identity_ca" in self.data:
+            raise forms.ValidationError("This field is required!", code='invalid')
+        ident = self.data["identity_ca"]
+        if ident == "":
+            raise forms.ValidationError("This field is required!", code='invalid')
+        return ident
+
+    @property
+    def is_server_identity_checked(self):
+        return self.cleaned_data["is_server_identity"]
+
+    @is_server_identity_checked.setter
+    def is_server_identity_checked(self, value):
+        self.initial["is_server_identity"] = value
+
+    @property
+    def ca_identity(self):
+        if self.is_server_identity_checked:
+            if 'gateway' not in self.cleaned_data:
+                raise Exception("No gateway has been found in this form!")
+            return self.cleaned_data['gateway']
+        else:
+            return self.cleaned_data['identity_ca']
+
+    @ca_identity.setter
+    def ca_identity(self, value):
+        self.initial["identity_ca"] = value
+
+    def fill(self, connection):
+        for remote in connection.server_remote.all():
+            sub = remote.subclass()
+            if isinstance(sub, CaCertificateAuthentication) or isinstance(sub, AutoCaAuthentication):
+                is_server_identity_checked = sub.ca_identity == connection.server_remote_addresses.first().value
+                self.is_server_identity_checked = is_server_identity_checked
+                if not is_server_identity_checked:
+                    self.ca_identity = sub.ca_identity
+
+    def create_connection(self, connection):
+        for remote in connection.server_remote.all():
+            sub = remote.subclass()
+            if isinstance(sub, AutoCaAuthentication) or isinstance(sub, CaCertificateAuthentication):
+                sub.ca_identity = self.ca_identity
+                sub.save()
+                return
+        raise Exception(
+            "No AutoCaAuthentication or CaCertificateAuthentication found that can be used to insert identity.")
+
+    def update_connection(self, connection):
+        for remote in connection.server_remote.all():
+            sub = remote.subclass()
+            if isinstance(sub, CaCertificateAuthentication) or isinstance(sub, AutoCaAuthentication):
+                sub.ca_identity = self.ca_identity
+                sub.save()
+
+
+class UserCertificateForm(forms.Form):
+    """
+    Form to choose the Usercertifite. Only shows the certs which contains a private key
+    """
+    certificate = CertificateChoice(queryset=UserCertificate.objects.none(), label="User certificate",
+                                    required=True)
+    identity = IdentityChoice(choices=(), required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCertificateForm, self).__init__(*args, **kwargs)
+        self.fields['certificate'].queryset = UserCertificate.objects.filter(private_key__isnull=False)
+
+    def update_certificates(self):
+        IdentityChoice.load_identities(self, "certificate", "identity")
+
+    @property
+    def my_certificate(self):
+        return UserCertificate.objects.get(pk=self.cleaned_data["certificate"])
+
+    @my_certificate.setter
+    def my_certificate(self, value):
+        self.initial['certificate'] = value
+        IdentityChoice.load_identities(self, "certificate", "identity")
+
+    @property
+    def my_identity(self):
+        return AbstractIdentity.objects.get(pk=self.cleaned_data["identity"])
+
+    @my_identity.setter
+    def my_identity(self, value):
+        self.initial['identity'] = value
+
+    def fill(self, connection):
+        local_auth = None
+        for local in connection.server_local.all():
+            subclass = local.subclass()
+            if isinstance(subclass, CertificateAuthentication):
+                local_auth = subclass
+                break
+        if local_auth is None:
+            assert False
+        self.my_certificate = local_auth.identity.certificate.pk
+        self.my_identity = local_auth.identity.pk
+
+    def create_connection(self, connection):
+        CertificateAuthentication(name='local', auth='pubkey', local=connection, identity=self.my_identity).save()
+
+    def update_connection(self, connection):
+        for local in connection.server_local.all():
+            sub = local.subclass()
+            if isinstance(sub, CertificateAuthentication):
+                sub.identity = self.my_identity
+                sub.save()
+
+
+class EapTlsForm(UserCertificateForm):
+    def fill(self, connection):
+        local_auth = None
+        for local in connection.server_local.all():
+            subclass = local.subclass()
+            if isinstance(subclass, EapTlsAuthentication):
+                local_auth = subclass
+                break
+        if local_auth is None:
+            assert False
+        self.my_certificate = local_auth.identity.certificate.pk
+        self.my_identity = local_auth.identity.pk
+
+    def create_connection(self, connection):
+        EapTlsAuthentication(name='local-eap-tls', auth='eap-tls', local=connection, identity=self.my_identity).save()
+
+    def update_connection(self, connection):
+        for local in connection.server_local.all():
+            sub = local.subclass()
+            if isinstance(sub, EapTlsAuthentication):
+                sub.identity = self.my_identity
+                sub.save()
+
+
+class EapForm(forms.Form):
+    """
+    Form to choose the username and password.
+    """
+    username = forms.CharField(max_length=50, initial="")
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput, initial="")
+
+    @property
+    def my_username(self):
+        return self.cleaned_data["username"]
+
+    @my_username.setter
+    def my_username(self, value):
+        self.initial['username'] = value
+
+    @property
+    def my_password(self):
+        return self.cleaned_data["password"]
+
+    @my_password.setter
+    def my_password(self, value):
+        self.initial['password'] = value
+
+    def fill(self, connection):
+        for local in connection.server_local.all():
+            subclass = local.subclass()
+            if isinstance(subclass, EapAuthentication):
+                self.fields['username'].initial = subclass.eap_id
+                self.fields['password'].initial = Secret.objects.filter(authentication=subclass).first().data
+
+    def create_connection(self, connection):
+        max_round = 0
+        for local in connection.server_local.all():
+            if local.round > max_round:
+                max_round = local.round
+
+        auth = EapAuthentication(name='local-eap', auth='eap', local=connection, eap_id=self.my_username, round=max_round + 1)
+        auth.save()
+        Secret(type='EAP', data=self.my_password, authentication=auth).save()
+
+    def update_connection(self, connection):
+        for local in connection.server_local.all():
+            sub = local.subclass()
+            if isinstance(sub, EapAuthentication):
+                sub.eap_id = self.my_username
+                sub.save()
+                Secret.objects.filter(authentication=sub).update(data=self.my_password)

strongMan/apps/server_connections/forms/__init__.py
@@ -1,0 +1,1 @@
+from .ConnectionForms import Ike2CertificateForm, Ike2EapTlsForm, IKEv2CertificateEAP, IKEv2EAP, IKEv2EapTls, ChooseTypeForm

strongMan/apps/server_connections/models/__init__.py
@@ -1,0 +1,3 @@
+from .connections import *
+from .authentication import *
+from .specific import *

commit 88ae4cd392f7805c81ab87f4a0a5480e3859f0b7

Properly prevent deletion of certificates used in server connections
Fixes: 52530499cfa1 ("Server connections added")
Closes #113.

strongMan/apps/server_connections/models/specific.py
@@ -10,8 +10,8 @@ from .common import CertConDoNotDeleteMessage, KeyConDoNotDeleteMessage
 @receiver(UserCertificate.should_prevent_delete_signal, sender=UserCertificate)
 def prevent_cert_delete_if_cert_is_in_use(sender, **kwargs):
     cert = kwargs['instance']
-    authentications = [ident.tls_identity for ident in cert.identities] + [ident.cert_identity for ident in
-                                                                           cert.identities] + \
+    authentications = [ident.server_tls_identity for ident in cert.identities] + \
+                      [ident.server_cert_identity for ident in cert.identities] + \
                       [cert.server_ca_cert_authentication]
 
     for auth in authentications:
@@ -23,8 +23,8 @@ def prevent_cert_delete_if_cert_is_in_use(sender, **kwargs):
 @receiver(PrivateKey.should_prevent_delete_signal, sender=PrivateKey)
 def prevent_key_delete_if_cert_is_in_use(sender, **kwargs):
     cert = kwargs['usercertificate']
-    authentications = [ident.tls_identity for ident in cert.identities] + [ident.cert_identity for ident in
-                                                                           cert.identities]
+    authentications = [ident.server_tls_identity for ident in cert.identities] + \
+                      [ident.server_cert_identity for ident in cert.identities]
     for auth in authentications:
         if auth.count() > 0:
             raise CertificateDoNotDelete(KeyConDoNotDeleteMessage(auth.first().connection))