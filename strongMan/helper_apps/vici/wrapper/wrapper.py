aba10e70ee56c2076ec78c9f3f214c6e9b651b8e
rc4 <dh@rc4.io>
2026-02-26 15:48:26 -0500

    Undo get_pool() check in unload_pool()

--- strongMan/helper_apps/vici/wrapper/wrapper.py
+++ strongMan/helper_apps/vici/wrapper/wrapper.py
@@ -233,9 +233,6 @@ class ViciWrapper(object):
             return self.session.get_pools()
 
     def unload_pool(self, pool_name):
-        pools = self.session.get_pools({"name": pool_name})
-        if pool_name not in pools:
-            return
         try:
             self.session.unload_pool({"name": pool_name})
         except Exception as e:
